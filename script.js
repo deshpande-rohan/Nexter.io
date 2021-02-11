94d24da65c4ff3d2b7f843172130e88ad3d9c2ca
rohan <funny12@gmail.com>
2021-02-11 12:08:33 +0530

    Completed Nexter Project

---
+++ script.js
@@ -0,0 +1,14 @@
+function x(){
+    for(var i=1; i<5; i++){
+        function close(x){
+            setTimeout(function(){
+                console.log(x);
+            }, x*1000);
+        }
+        close(i);
+    }
+
+    console.log("here is goes !");
+}
+
+x();
